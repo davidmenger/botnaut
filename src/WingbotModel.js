f52d665d7c0d9e5ef13cf4fbca6214424e27d4f4
David Menger <david.menger@me.com>
2017-10-23 10:34:57 +0200

    Prolonged timeout

--- src/WingbotModel.js
+++ src/WingbotModel.js
@@ -76,7 +76,7 @@ class WingbotModel {
             uri: `${this._options.serviceUrl}/${this._options.model}`,
             qs,
             json: true,
-            timeout: 20000
+            timeout: 2000
         }).then((response) => {
 
             if (response.error || !Array.isArray(response.tags)) {
