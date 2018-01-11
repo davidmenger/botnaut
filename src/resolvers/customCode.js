61790ae2b2b4779581c5004a9e67439dc62a89c3
David Menger <david.menger@me.com>
2018-01-11 13:24:00 +0100

    Added automatic return of custom code

--- src/resolvers/customCode.js
+++ src/resolvers/customCode.js
@@ -6,7 +6,7 @@
 function customCode (params, context, blocks) {
     const customFn = blocks.getCustomCodeFactory(params.codeBlockId);
 
-    const { router } = context;
+    const { router, isLastIndex } = context;
 
     const items = Object.keys(params.items)
         .reduce((obj, itemName) => {
@@ -19,7 +19,7 @@ function customCode (params, context, blocks) {
 
     const paramsData = typeof params.params === 'object' ? params.params : {};
 
-    return (req, res, postBack, path, action) => {
+    return function* (req, res, postBack, path, action) {
 
         Object.assign(res, {
             run (codeBlockName) {
@@ -33,7 +33,16 @@ function customCode (params, context, blocks) {
         });
 
         // assign to res
-        return customFn(req, res, postBack, context, paramsData);
+        let ret = customFn(req, res, postBack, context, paramsData);
+
+        if (typeof ret === 'object' && ret !== null) {
+            ret = yield ret;
+        }
+
+        if (typeof ret !== 'undefined') {
+            return ret;
+        }
+        return isLastIndex ? null : true;
     };
 }
 
